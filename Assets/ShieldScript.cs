e719c576d3fad28f7affa7504c7b859b54368ccc
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-06-01 16:52:47 +0600

    initial commit

---
+++ Assets/ShieldScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+	
+	public GameObject Shield;
+	private bool isShield=false;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shield.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0)){
+        	isShield=!isShield;
+        	Shield.SetActive(isShield);
+        } 
+    }
+}
